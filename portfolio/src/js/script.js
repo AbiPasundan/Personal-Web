71e0454720865b8a358e4e4b352209345cf6be25
Muhamad Wildan Nursyamsi Abdullah Al-Harist <125271650+AbiPasundan@users.noreply.github.com>
2024-09-08 22:08:15 +0700

    drag and drop
    adding all of the file

---
+++ portfolio/src/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    // responsive navbar start
+    $("#on").click(function() {
+        $(".bgk").css({
+            "transform": "translateX(0)"
+        })
+    })
+    $(".close").click(function() {
+        $(".bgk").css({
+            "transform": "translateX(150%)"
+        })
+    })
+    $(".close").click(function(){
+        $(".bgk").css({
+            "transform": "translateX(150%)"
+        })
+    })
+    // responsive navbar end
+    $("#dummy").click(function() {
+        // $("#theme")
+    })
+})
